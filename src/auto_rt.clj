36e9cbeae44b350478919f88ba1d8329071ce6f2
Sam Neubardt <samneubardt@gmail.com>
2012-09-16 20:01:06 -0400

    Add env helper function
    Retrieves the value of an environment var

--- src/auto_rt.clj
+++ src/auto_rt.clj
@@ -1,4 +1,11 @@
-(ns auto-rt.core)
+(ns auto-rt.core
+  (:require [twitter.oauth :as oauth]
+            [twitter.api.restful :as twitter]))
+
+(defn env
+  "Retrieve the value of var-name from the system environment, or nil"
+  [var-name]
+  (System/getenv var-name))
 
 (defn -main
   "I don't do a whole lot."
