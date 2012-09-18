b8852666247c43457e3c42cc11ac8562c2a45288
Sam Neubardt <samneubardt@gmail.com>
2012-09-17 22:35:02 -0400

    Add :main and min lein ver

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject auto-rt "0.1.0-SNAPSHOT"
   :description "Poll for mentions and retweet"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"                     
+  :main auto-rt 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
