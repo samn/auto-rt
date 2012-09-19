e39c4b8e6acf630c09315f7d11cfa20fa70bf18a
Sam Neubardt <samneubardt@gmail.com>
2012-09-19 00:01:18 -0400

    Correctly ignore non @mentions

--- src/auto_rt.clj
+++ src/auto_rt.clj
@@ -26,10 +26,9 @@
   "Called when a new message is received from the streaming api"
   [response baos]
   (let [tweet (json/parse-string (.toString baos) true)]
-    (println "*user-id*" *user-id*)
-    (println "(get-in tweet [:user :id_str])" (get-in tweet [:user :id_str]))
-    (when (not= *user-id* (get-in tweet [:user :id_str]))
-      (restful/retweet-status :oauth-creds *creds* :params {:id (:id_str tweet)}))))
+    (when-let [user (:user tweet)]
+      (when (not= *user-id* (:id_str user))
+        (restful/retweet-status :oauth-creds *creds* :params {:id (:id_str tweet)})))))
 
 (defn on-failure
   "Called when the streaming api returns a 4xx response"
