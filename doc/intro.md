39a257f7c622cbde6de00cea84444cc836bd510d
Sam Neubardt <samneubardt@gmail.com>
2012-09-20 22:41:23 -0400

    No doc or tests for now :(

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to auto-rt
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/auto_rt/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns auto-rt.core-test
-  (:use clojure.test
-        auto-rt.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
